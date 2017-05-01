07dddb318cc20252f780b707a659f2cd3615dec9
Kat Catalano <katrina.catalano@gmail.com>
2017-04-30 21:28:40 -0400

    BOOM

--- mapping.R
+++ mapping.R
@@ -4,6 +4,8 @@ setwd("~/Documents/GradSchool/parentage")
 source("~/Documents/GradSchool/parentage/samplefromlig.R")
 source("~/Documents/GradSchool/parentage/conleyte.R")
 source("~/Documents/GradSchool/parentage/conlabor.R")
+source("~/Documents/GradSchool/parentage/katPlot.R")
+
 suppressMessages(library(dplyr))
 library(lubridate)
 library(stringr)
@@ -13,7 +15,12 @@ library(maptools)
 library(geosphere)
 library(igraph)
 library(sp)
-
+library(RColorBrewer)
+library(igraph)
+library(scales)
+library(ggplot2)
+library(ggmap)
+library(plotrix)
 #need to get the lat long id for all parentage matches and map, then use igraph to connect results
 
 leyte <- conleyte()
@@ -33,9 +40,9 @@ coord <- unique(coor)
 
 #load tables with parent offspring matches and attach lat lon
 #prepare full likelihood parentage/sibship results for mapping
-fullsib <- read.table(file= "174HQloci_adult_sep_2013WP1.FullSibDyad.txt", header= TRUE)
+fullsib <- read.table(file= "174HQ_parentage_2014.FullSibDyad.txt", header= TRUE)
 halfsib <- read.table(file= "174HQloci_adult_sep_2013WP1.HalfSibDyad.txt", header= TRUE)	
-pairs <- read.table(file= "174HQloci_adult_sep_2013WP1.Maternity.txt", header= TRUE)
+pairs <- read.table(file= "174HQ_parentage_2014.Maternity.txt", header= TRUE)
 
 #fs <- sampfromlig(fullsib[1])#for some reason, not getting all the sample ids with this method, going to try old way
 #fs2 <- sampfromlig(fullsib[2])
@@ -117,9 +124,9 @@ pairs <- left_join(pairs, second, by=c(par.sample_id="par.Sample_ID"))
 
 
 #clean up, remove unnecessary columns
-pairs <- pairs %>% select(OffspringID, InferredMum1, offs.ObsTime, offs.date, par.ObsTime, par.date, offs.Size, par.Size, ProbMum1)#, GPS)
-fullsib <- fullsib %>% select(sib1, sib2, sib1.ObsTime, sib1.date, sib2.ObsTime, sib2.date, sib1.Size, sib2.Size, Probability)#, GPS)
-halfsib <- halfsib %>% select(hsib1, hsib2, hsib1.ObsTime, hsib1.date, hsib2.ObsTime, hsib2.date, hsib1.Size, hsib2.Size, Probability)#, GPS)
+pairs <- pairs %>% select(OffspringID, InferredMum1, offs.ObsTime, offs.date, par.ObsTime, par.date, offs.Size, par.Size, ProbMum1, par.name, offs.name)#, GPS)
+fullsib <- fullsib %>% select(sib1, sib2, sib1.ObsTime, sib1.date, sib2.ObsTime, sib2.date, sib1.Size, sib2.Size, Probability, sib1.name, sib2.name)#, GPS)
+halfsib <- halfsib %>% select(hsib1, hsib2, hsib1.ObsTime, hsib1.date, hsib2.ObsTime, hsib2.date, hsib1.Size, hsib2.Size, Probability, hsib1.name, hsib2.name)#, GPS)
 
 #attach lat/lon for parentage
 #idcsv <- data.frame(read.csv("174_colonypars_sibs_adultsep13.csv", header = TRUE), stringsAsFactors = FALSE)	
@@ -191,7 +198,7 @@ pairlatlon <- left_join(pairlatlon, latlongp, by = c(pmonth="par.month", pday="p
 pairlatlon <- pairlatlon %>%
   #select(-sec.y) %>%  # remove sec.y column
   #%>% # keep all unique observance events (need that id col in the excel sheet) %>%
- select(OffspringID, InferredMum1, offs.lat, offs.lon, par.lat, par.lon, ProbMum1)
+ select(OffspringID, InferredMum1, offs.lat, offs.lon, par.lat, par.lon, offs.name, par.name, ProbMum1)
   #rename(sec = sec.x)
 
 
@@ -264,7 +271,7 @@ fslatlon <- left_join(fslatlon, latlongp, by = c(pmonth="sib2.month", pday="sib2
 fslatlon<- fslatlon %>%
   #select(-sec.y) %>%  # remove sec.y column
  #%>% # keep all unique observance events (need that id col in the excel sheet) %>%
- select(sib1, sib2, sib1.lat, sib1.lon, sib2.lat, sib2.lon, Probability)
+ select(sib1, sib2, sib1.lat, sib1.lon, sib2.lat, sib2.lon, sib1.name, sib2.name, Probability)
 #rename(sec = sec.x)
 
 
@@ -338,7 +345,7 @@ hslatlon <- left_join(hslatlon, latlongp, by = c(pmonth="hsib2.month", pday="hsi
 hslatlon<- hslatlon %>%
   #select(-sec.y) %>%  # remove sec.y column
  # %>% # keep all unique observance events (need that id col in the excel sheet) %>%
-select(hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon, Probability)
+select(hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon, hsib1.name, hsib2.name, Probability)
    #rename(sec = sec.x)
 
 
@@ -346,7 +353,7 @@ select(hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon, Probability)
 
 
 
-#make a map with connectivity
+#make a map with connectivity of INDIVIDUALS
 #start by defining edges and vertices
 #first get rid of low probability matches
 fslatlon <-filter(fslatlon, Probability > 0.98)
@@ -358,151 +365,187 @@ pairlatlon <- pairlatlon %>% group_by(OffspringID, InferredMum1) %>% filter(row_
 
 	
 edgesp <- data.frame(select(pairlatlon, OffspringID, InferredMum1, offs.lat, offs.lon, par.lat, par.lon), stringsAsFactors = FALSE)
-edgesp$type <- "parent"
+#edgesp$type <- "parent"
 edgesfs <- data.frame(select(fslatlon, sib1, sib2, sib1.lat, sib1.lon, sib2.lat, sib2.lon), stringsAsFactors = FALSE)
-edgesfs$type <- "fullsib"
-edgeshs <- select(hslatlon, hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon)
-edgeshs$type <- "halfsib"
-names(edgesp) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "type")
-names(edgesfs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "type")
-names(edgeshs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "type")
+#edgesfs$type <- "fullsib"
+edgeshs <- data.frame(select(hslatlon, hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon), stringsAsFactors = FALSE)
+#edgeshs$type <- "halfsib"
+names(edgesp) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon")
+names(edgesfs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon")
+names(edgeshs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon")
 edges1 <- bind_rows(edgesp, edgesfs)
 edges2 <- bind_rows(edges1, edgeshs)
 
+#now nodes need to be in format (id, lon, lat), then make layout from nodes and edges need to be (one, two)
 
-nodes1 <- select(edgesp, one)
-nodes2 <- select(edgesp, two)
-nodes3 <- select(edgesfs, one)
-nodes4 <- select(edgesfs, two)
-nodes5 <- select(edgeshs, one)
-nodes6 <- select(edgeshs, two)
-nodesa <- full_join(nodes1, nodes2, by=c(one = "two"))
-nodesb <-full_join(nodesa, nodes3, by=c(one = "one"))
-nodesc <-full_join(nodesb, nodes4, by=c(one = "two"))
-nodesd <-full_join(nodesc, nodes5, by=c(one = "one"))
-nodese <-full_join(nodesd, nodes6, by=c(one = "two"))
-
-#remove duplicate vertexs and nodes
-nodes <- distinct(nodesc)
 #group by the ids that repeat, and then only keep the first line of them, elminates duplicates
 edges <- edges2 %>% group_by(one, two) %>% filter(row_number() == 1) 
-edges <- distinct(edges2)
-matches <- edges 
-matches <- matches %>% select(one, two, type) 
-nodes <- edges %>% group_by(one) %>% filter(row_number() == 1) 
-edges <- matches%>% group_by(one, two) %>% filter(row_number() == 1) 
 
-matches <- distinct(matches)
-write.csv(edges, file="2013edges.csv", col.names=TRUE)
+nodesone <- edges %>% group_by(one) %>% filter(row_number() == 1) 
+nodestwo <- edges %>% group_by(two) %>% filter(row_number() == 1) 
+nodes <- full_join(nodesone, nodestwo)
 
-#create data frame OR
-nodes <- edges$one
-x <- edges$one.lat
-y <- edges$one.lon
-from <- matches$one
-to <- matches$two
-NodeList <- data.frame(nodes, x ,y)
-EdgeList <- data.frame(from, to)
+edges <- edges%>% group_by(one, two) %>% filter(row_number() == 1) 
+edges <- data.frame(select(edges, one, two), stringsAsFactors= FALSE)
 
-#just load data frames
-nodes <- read.csv(file="2013edges.csv", header=TRUE)
-matches <- read.csv(file="2013matches.csv", header=TRUE)
-
-#remove duplicates
-edges <- matches%>% group_by(one, two) %>% filter(row_number() == 1) 
-nodes <- nodes %>% group_by(id) %>% filter(row_number() == 1) 
+nodes <- as.data.frame(nodes)
+nodes1 <- data.frame(select(nodes, one, one.lon, one.lat), stringsAsFactors= FALSE)
+nodes2 <- data.frame(select(nodes, two, two.lon, two.lat), stringsAsFactors= FALSE)
+names(nodes2) <- c("one", "one.lon", "one.lat")
+nodes <- bind_rows(nodes1, nodes2)
+nodes <-distinct(nodes)
 
 
+meta <- nodes
+lo <- as.matrix(meta[,2:3])
+#get rid of quotes, delete first column when you write csv
+write.csv(lo, file="lo2014.csv", col.names=TRUE)
+lo <- read.csv(file="lo2014.csv", header=TRUE)
+
+
+#read in high res leyte map to map lat/long points on
 library(rgdal)
 map_new <- readOGR("leyte", "coastlines_leyte")
 
-plot(map_new)
-#read in high res leyte map to map lat/long points on
-meta <- nodes
-lo <- as.matrix(meta[,2:3])
-plot(nodes$x, nodes$y, type="n")
+plot(map_new, xlim=c(124.63, 124.8 ), ylim=c(10.8,10.83))
+
+
 a<- graph_from_data_frame(vertices = nodes, d= edges, directed = TRUE)
-plot(a, rescale=FALSE, asp=0, add=TRUE, edge.arrow.size=.1, vertex.size=.25, vertex.frame.color="gray", 
-		vertex.label.color="black", vertex.label.cex=0.2, vertex.label.dist=0, edge.width=.5, 
-		edge.color=c("orange", "green", "blue")[(E(a)$type=="parent")+2], layout=lo)
-
-plot(map_new, xlim=c(124.684279, 124.829 ), ylim=c(10.621,10.999))
-
-xlim=c(124.684279, 124.829 ), ylim=c(10.621,10.999),
+plot(a, rescale=FALSE, asp=0, add=TRUE, edge.arrow.size=.2, vertex.size=.4, vertex.frame.color=NA, 
+	 vertex.label.cex=.6, vertex.label.degree=pi, vertex.label.dist=.08, edge.width=edges$number, 
+		edge.color="slate grey", layout=lo, edge.curved=.1, vertex.label.font=2, vertex.label.color="gray40")
+#TO DO: MAKE A MAP OF SIBLING AND HALFSIBLING RELATEDNESS ACCORING TO SITE, RUN MIGEST
 
 
+#map connections by location
+edgesp <- data.frame(select(pairlatlon, OffspringID, InferredMum1, offs.lat, offs.lon, par.lat, par.lon, par.name, offs.name), stringsAsFactors = FALSE)
+edgesfs <- data.frame(select(fslatlon, sib1, sib2, sib1.lat, sib1.lon, sib2.lat, sib2.lon, sib1.name, sib2.name), stringsAsFactors = FALSE)
+edgeshs <- data.frame(select(hslatlon, hsib1, hsib2, hsib1.lat, hsib1.lon, hsib2.lat, hsib2.lon, hsib1.name, hsib2.name), stringsAsFactors=FALSE)
+names(edgesp) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "one.name", "two.name")#"type")
+names(edgesfs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "one.name", "two.name")# "type")
+edgesfs<- edgesfs %>% group_by(one, two) %>% filter(row_number() == 1) 
+names(edgeshs) <- c("one", "two", "one.lat", "one.lon", "two.lat","two.lon", "one.name", "two.name")# "type")
+edgeshs<- edgeshs %>% group_by(one, two) %>% filter(row_number() == 1) 
+
+#for parents
+edgesp$one <-edgesp$one.name
+edgesp$two <- edgesp$two.name
+nodesp <-edgesp[,1:6]
+nodesp1 <-nodesp %>% select(one, one.lon, one.lat)
+nodesp2 <-nodesp %>% select(two, two.lon, two.lat)
+names(nodesp2) <- c("one", "one.lon", "one.lat")
+nodes <- bind_rows(nodesp1, nodesp2)
+
+#rearrange # nodes <- nodes[c(1,3,2)]
+nodesp<- nodes %>% group_by(one) %>% filter(row_number() == 1) 
+
+#the lo and nodes is weird lat/lon, so load the edits csv where I fixed the spacing and have all of the labels, then join with the nodes
+edits <- read.csv(file="editlabels13.csv", header=TRUE)
+nodes1 <-inner_join(nodes, edits, by=c(one="site"))
+nodes1 <- select(nodes1, one, lon, lat)
+nodesp<- nodes1 %>% group_by(one) %>% filter(row_number() == 1) 
+
+meta <- nodesp
+lo <- as.matrix(meta[,2:3])
+
+#get rid of quotes, delete first column when you write csv
+write.csv(lo, file="lo2014locationsp.csv", col.names=TRUE)
+lo <- read.csv(file="lo2014locationsp.csv", header=TRUE)
+
+edgesp <- edgesp %>% group_by(one, two) %>% filter(row_number() == 1) 
+edgesp <- edgesp %>% count(one, two)
+
+plot(map_new, xlim=c(124.7, 124.8 ), ylim=c(10.65,10.85))
+
+a <- graph_from_data_frame(d=edgesp, vertices=nodesp, directed=TRUE)
+
+katPlot(a, rescale=FALSE, asp=0, add=TRUE, edge.arrow.size=.2, vertex.size=.4, vertex.frame.color=NA, 
+	 vertex.label.cex=.7, vertex.color= "orange", vertex.label.degree=pi, vertex.label.dist=.1, edge.width=edgesp$n,
+		edge.color="slate gray", layout=lo, edge.curved=.4, vertex.label.font=2, vertex.label.color="black")
+
+#plot siblings and half siblings on a map as points with graduated size to show density of siblings
+edgesfs$one <-edgesfs$one.name
+edgesfs$two <- edgesfs$two.name
+nodesfs <-edgesfs[,1:6]
+nodesfs <-as.data.frame(nodesfs)
+nodesfs1 <-nodesfs %>% select(one, one.lon, one.lat)
+nodesfs2 <-nodesfs %>% select(two, two.lon, two.lat)
+names(nodesfs2) <- c("one", "one.lon", "one.lat")
+nodesfs <- bind_rows(nodesfs1, nodesfs2)
+nodesfs1 <- nodesfs %>% count(one) 
+nodesfs <-left_join(nodesfs, nodesfs1)
+nodesfs<- nodesfs %>% group_by(one) %>% filter(row_number() == 1) 
+
+#gather 
+plot(map_new, xlim=c(124.7, 124.8 ), ylim=c(10.60,10.89))
+points(nodesfs$one.lon, nodesfs$one.lat, pch=10, col=alpha("dark blue", 0.3), cex=nodesfs$n/6)
+points(nodesfs$one.lon, nodesfs$one.lat, pch=19, col=alpha("blue", 0.3), cex=nodesfs$n/6)
+
+#move points for crunchy sites
+
+edits <- read.csv(file="editlabels13.csv", header=TRUE)
+
+text(edits$lon, edits$lat, labels=edits$site, col="black", cex=.6, offset=.2, pos=2)
+
+#for half siblings
+edgeshs$one <-edgeshs$one.name
+edgeshs$two <- edgeshs$two.name
+nodeshs <-edgeshs[,1:6]
+nodeshs <-as.data.frame(nodeshs)
+nodeshs1 <-nodeshs %>% select(one, one.lon, one.lat)
+nodeshs2 <-nodeshs %>% select(two, two.lon, two.lat)
+names(nodeshs2) <- c("one", "one.lon", "one.lat")
+nodeshs <- bind_rows(nodeshs1, nodeshs2)
+nodeshs1 <- nodeshs %>% count(one) 
+nodeshs <-left_join(nodeshs, nodeshs1)
+nodeshs<- nodeshs %>% group_by(one) %>% filter(row_number() == 1) 
+
+#gather 
+plot(map_new, xlim=c(124.7, 124.8 ), ylim=c(10.60,10.89))
+points(nodeshs$one.lon, nodeshs$one.lat, pch=10, col=alpha("dark blue", 0.3), cex=nodeshs$n/6)
+points(nodeshs$one.lon, nodeshs$one.lat, pch=19, col=alpha("blue", 0.3), cex=nodeshs$n/6)
+
+#move points for crunchy sites
+
+edits <- read.csv(file="editlabels13.csv", header=TRUE)
+
+text(edits$lon, edits$lat, labels=edits$site, col="black", cex=.6, offset=.2, pos=2)
 
 
-edge.color=c("orange", "green", "blue")[(E(a)$type=="parent")+2],
-
-fsub <- edges[edges$type == "parent",]
-for (j in 1:length(edges)) {
-    nodes1 <- edges[edges$one == fsub[j,]$one,]
-    nodes2 <- edges[edges$two == fsub[j,]$two,]
-
-    inter <- gcIntermediate(c(edges[1,]$one.lon, edges[1,]$one.lat), c(edges[1,]$two.lon, edges[1,]$two.lat), n=7, addStartEnd=TRUE)
-      
-    lines(inter, col="black", lwd=0.8)
-}
-
-for(i in 1:nrow(flights))  {
-    node1 <- airports[airports$ID == flights[i,]$Source,]
-    node2 <- airports[airports$ID == flights[i,]$Target,]
-    
-    arc <- gcIntermediate( c(node1[1,]$longitude, node1[1,]$latitude), 
-                           c(node2[1,]$longitude, node2[1,]$latitude), 
-                           n=1000, addStartEnd=TRUE )
-    edge.ind <- round(100*flights[i,]$Freq / max(flights$Freq))
-    
-    lines(arc, col=edge.col[edge.ind], lwd=edge.ind/30)
-}
-	   
-   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
-	   
 
 
-library(marmap) ; library(mapdata);library(RColorBrewer);library(classInt)
-dat <- getNOAA.bathy(-78,-64,37,43,res=1, keep=TRUE)
-# Create color palettes
-blues <- c("lightsteelblue4", "lightsteelblue3", "lightsteelblue2", "lightsteelblue1")
-greys <- c(grey(0.6), grey(0.93), grey(0.99))
-plot(dat, xlim=c(-76.5,-65.5), ylim=c(38,41.5),im=TRUE, land=TRUE, bpal=list(c(min(dat),0,blues),c(0,max(dat),greys)), lwd=.09, las=1,cex.lab=1.75, cex.axis=1.75, cex.main=1.75, cex.sub=1.75 )
-map("worldHires", res=0, lwd=0.7, add=TRUE)
-plot(dat, deep=-25, shallow=-25, step=0, lwd=0.1, drawlabel=TRUE, add=TRUE)
-plot(dat, deep=-50, shallow=-50, step=0, lwd=0.1, drawlabel=TRUE, add=TRUE)
-plot(dat, deep=-75, shallow=-75, step=0, lwd=0.1, drawlabel=TRUE, add=TRUE)
-plot(dat, deep=-100, shallow=-100, step=0, lwd=0.1, drawlabel=TRUE, add=TRUE)
+#connectivity for siblings (kinda messy)
+edgesfs$one <-edgesfs$one.name
+edgesfs$two <- edgesfs$two.name
+nodesfs <-edgesfs[,1:6]
+nodesfs1 <-nodesfs %>% select(one, one.lon, one.lat)
+nodesfs2 <-nodesfs %>% select(two, two.lon, two.lat)
+names(nodesfs2) <- c("one", "one.lon", "one.lat")
+nodes <- bind_rows(nodesfs1, nodesfs2)
 
-coocNet<-network(edges,
-                 matrix.type='edgelist',
-                 directed=FALSE,  # this will be an undirected network
-                 ignore.eval=TRUE,  # confusingly, this tells it to include edge weights
-                 )
-				
-plot.network(coocNet,  # pass in the network
-# don't erase the map before drawing the network
-new=FALSE, 
-# get coordiantes from vertices and pass in as 2-col matrix
-coord=cbind(coocNet%v%'lon',coocNet%v%'lat'),  
-# ---- all the rest of these are optional to make it look nice ------
- # set a semi-transparent edge color
- edge.col='#AA555555',
- # specifiy an edge width scaled as fraction of total co-occurence
- edge.lwd=coocNet%e%'Tot_cooc'/500,
- # set the vertex size
- vertex.cex=0.5,
- # set a semi transparent vertex color
- vertex.col='#AA555555',
- vertex.border='white',
- # please don't jitter the points around
- jitter=FALSE)
+#rearrange # nodes <- nodes[c(1,3,2)]
+nodesfs<- nodes %>% group_by(one) %>% filter(row_number() == 1) 
 
+#the lo and nodes is weird lat/lon, so load the edits csv where I fixed the spacing and have all of the labels, then join with the nodes
+edits <- read.csv(file="editlabels13.csv", header=TRUE)
+nodes1 <-inner_join(nodesfs, edits, by=c(one="site"))
+nodes1 <- select(nodes1, one, lon, lat)
+nodesfs<- nodes1 %>% group_by(one) %>% filter(row_number() == 1) 
 
+meta <- nodesfs
+lo <- as.matrix(meta[,2:3])
+
+#get rid of quotes, delete first column when you write csv
+write.csv(lo, file="lo2014locationsfs.csv", col.names=TRUE)
+lo <- read.csv(file="lo2014locationsfs.csv", header=TRUE)
+
+edgesfs <- edgesfs %>% group_by(one, two) %>% filter(row_number() == 1) 
+edgesfs <- edgesfs %>% count(one, two)
+
+plot(map_new, xlim=c(124.7, 124.8 ), ylim=c(10.65,10.85))
+
+a <- graph_from_data_frame(d=edgesfs, vertices=nodesfs, directed=TRUE)
+
+katPlot(a, rescale=FALSE, asp=0, add=TRUE, edge.arrow.size=.2, vertex.size=.4, vertex.frame.color=NA, 
+	 vertex.label.cex=.7, vertex.color= "orange", vertex.label.degree=pi, vertex.label.dist=.1, edge.width=edgesp$n,
+		edge.color="slate gray", layout=lo, edge.curved=.4, vertex.label.font=2, vertex.label.color="black")
