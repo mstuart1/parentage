e9cadf2e2247e920cae8f32f3fbaa7d01a3663f4
katcat <katrina.catalano@gmail.com>
2017-10-10 08:26:04 -0400

    updates

--- dispersalhist.R
+++ dispersalhist.R
@@ -20,7 +20,7 @@ labor <- conlabor()
 
 #load tables with parent offspring matches and attach lat lon
 #prepare full likelihood parentage/sibship results for mapping
-pairs <- read.table(file= "20170831colony12.Maternity.txt", header= TRUE)
+pairs <- read.table(file= "20170927colony15.Maternityc.txt", header= TRUE)
 
 # add lab IDs
 
@@ -29,30 +29,37 @@ c2 <- labor %>% tbl("digest") %>% select(digest_id, extraction_id)
 c3 <- left_join(c2, c1, by = "extraction_id")
 c4 <- labor %>% tbl("ligation") %>% select(ligation_id, digest_id)
 labid <- collect(left_join(c4, c3, by = "digest_id"))
-
+head(labid)
 first <- labid
 names(first) <- paste("offs.", names(first), sep = "")
 pairs <- left_join(pairs, first, by=c(OffspringID="offs.ligation_id"))
 sec <- labid
 names(sec) <- paste("par.", names(sec), sep = "")
 pairs <- left_join(pairs, sec, by=c(InferredMum1="par.ligation_id"))
-
-c1 <- leyte %>% tbl("diveinfo") %>% select(dive_table_id, date, site, gps)
+head(pairs)
+c1 <- leyte %>% tbl("diveinfo") %>% select(dive_table_id, date, site, gps, dive_type)
 c2 <- leyte %>% tbl("anemones") %>% select(dive_table_id, anem_table_id, obs_time)
 c3 <- as.data.frame(left_join(c2, c1, by ="dive_table_id"))
 c4 <- as.data.frame(tbl(leyte, sql("SELECT fish_table_id, anem_table_id, sample_id, size FROM clownfish where sample_id is not NULL")))
 fieldid <- as.data.frame((left_join(c4, c3, by = "anem_table_id")))
-
+dim(fieldid)
+head(fieldid)
+#don't need this line in 2015, only clownfish dives that year
+stuff <- fieldid %>% group_by(dive_type) %>% filter(dive_type=="C")
 #now add parent info
-
-first <- fieldid
+ungroup(stuff)
+head(stuff)
+first <- stuff
 names(first) <- paste("offs.", names(first), sep = "")
+head(first)
 pairs <- left_join(pairs, first, by="offs.sample_id")
-second <- fieldid
+head(pairs)
+second <- stuff
+head(second)
 names(second) <- paste("par.", names(second), sep = "")
 pairs <- left_join(pairs, second, by="par.sample_id")
-#2014 L1250 looses it's site name in this process... can't figure out why. If you go back through the script you can see L1250 came from Carirdad Proper
-
+#2015 L1550 looses it's site name in this process... can't figure out why. If you go back through the script you can see L1550 came from Carirdad Proper
+head(pairs)
 #clean up, remove unnecessary columns
 pairs <- pairs %>% select(OffspringID, InferredMum1, offs.obs_time, offs.date, par.obs_time, par.date, offs.size, par.size, ProbMum1, par.site, offs.site, par.gps, offs.gps)
 
@@ -88,7 +95,7 @@ pairs <- pairs %>%
 
 #trim the data to keep only time and parent offspring matches, and then elminate repeats
 pairs <- distinct(pairs) #seems to be no need for this line but keep it in here as a reminder if I run into other problems
-
+head(pairs)
 #20170905 checked through here... thinking problem is in the gps unit number matching
 latlong <- data.frame(leyte %>% tbl("GPX") %>% collect(n = Inf))
 latlong$time <- ymd_hms(latlong$time)
@@ -116,7 +123,7 @@ latlongp <- latlong %>%
 
 names(latlongp) <- paste("par.", names(latlongp), sep = "")
 pairlatlon <- left_join(pairlatlon, latlongp, by = c(pyear="par.year", pmonth="par.month", pday="par.day", phour="par.hour", pmin="par.min",  par.gps="par.unit"))
-
+head(pairlatlon)
 #now trim to just necessary columns
 #### HERE CAN I USE GROUP BY ID OR OBSTIME AND THEN SUMMARISE TO GET THE MEAN LAT LON OR MIN LAT LON AND CREATE A NEW TABLE WITH ALL COLUMNS BUT ONLY ONE LAT LON PER OBS
 # because all of the lat longs for the 4 observations are basically the same, remove sec.y and find distinct values
@@ -126,7 +133,7 @@ pairlatlon <- pairlatlon %>%
  select(OffspringID, InferredMum1, offs.lat, offs.lon, par.lat, par.lon, offs.site, par.site, ProbMum1) %>% filter(ProbMum1 >= .98)
   #rename(sec = sec.x)
 pairlatlon<- pairlatlon %>% group_by(OffspringID, InferredMum1) %>% filter(row_number() == 1) 
-
+head(pairlatlon)
 pairlatlon$offs.lat <- as.numeric(pairlatlon$offs.lat)
 pairlatlon$offs.lon <- as.numeric(pairlatlon$offs.lon)
 pairlatlon$par.lat <- as.numeric(pairlatlon$par.lat)
@@ -134,9 +141,9 @@ pairlatlon$par.lon <- as.numeric(pairlatlon$par.lon)
 
 alldists <- rdist.earth(as.matrix(pairlatlon[,c('offs.lon', 'offs.lat')]), as.matrix(pairlatlon[,c('par.lon', 'par.lat')]), miles=FALSE, R=6371)
 pairlatlon$distkm <- diag(alldists)
-
+head(pairlatlon)
 #write distance files
-write.csv(pairlatlon, file="pairdist2012.csv")
+write.csv(pairlatlon, file="pairdist2015.csv")
 #calculate the distance between all anemones
 #make a matrix with all anemones and lat lon
 
@@ -173,6 +180,8 @@ anems <- anems %>%
 
 anems1 <- left_join(anems, latlong, by=c("year", "month", "day", "hour", "min", "sec", gps="unit"))
 anems2 <- anems1 %>% filter(lat != 'NA' & lon != 'NA')
+alldists <- rdist.earth(as.matrix(anems2[,c('lat', 'lon')]), miles=FALSE, R=6371)
+anems2$distkm <- diag(alldists)
 
 #write out table
 write.csv(anems2, file="anemdist.csv")
@@ -196,12 +205,21 @@ fourteen$year <- '2014'
 fifteen$year <- '2015'
 
 all <- bind_rows(twelve, thirteen, fourteen, fifteen)
-all <- all %>% select(-X, -X.1)
-
+all <- all %>% select(-X)
+head(all)
 #find means 
 all$year <- as.character(all$year)
-
-ggplot(all, aes(x=distkm, fill=year)) +
+means <- as.data.frame(aggregate(distkm ~ year, all, mean))
+#find average dispersal mean
+allmean <- mean(all$distkm)
+#make a matrix of average mean, add it to the annual averages
+addmean <- as.data.frame(allmean)
+addmean$year <- "all"
+colnames(addmean)<- c("distkm", "year")
+allmean2 <- bind_rows(means, addmean)
+head(allmean2)
+#plot average dispersal distances
+ggplot(allmean2, aes(x=distkm, fill=year)) +
     geom_histogram(binwidth=1, alpha=.5, position="dodge")
 	
 #plot the distances between sites
@@ -210,15 +228,15 @@ ggplot(all, aes(x=distkm, fill=year)) +
 dist <- read.csv("sitedist.csv", header=TRUE)
 dist$year <- 'site-site distance'
 dist$OffspringId <- seq(from=1, to=137, by=1)
-dist$InferredMum1 <- NA
-dist$offs.lat <- NA
-dist$offs.lon <- NA
-dist$par.lat <- NA
-dist$par.lon <- NA
-dist$offs.site <- NA
-dist$par.site <- NA
-dist$ProbMum1 <- NA
-
+dist$InferredMum1 <- NULL
+dist$offs.lat <- NULL
+dist$offs.lon <- NULL
+dist$par.lat <- NULL
+dist$par.lon <- NULL
+dist$offs.site <- NULL
+dist$par.site <- NULL
+dist$ProbMum1 <- NULL
+head(dist)
 #bin counts of possible distances in the population
 dist1 <-as.data.frame(round(dist$distkm, digits=0))
 names(dist1) <- "distkm"
@@ -231,7 +249,6 @@ all2 <-as.data.frame(round(all2$distkm, digits=0))
 names(all2) <- "distkm"
 all2$distkm <- as.character(all2$distkm)
 all2 <- all2 %>% group_by(distkm) %>% count()
-means <- aggregate(distkm ~ year, all, mean)
 colors <- c("red", "blue", "orange", "green")
 
 dist$rounded <-round(dist$distkm, digits=0)
@@ -241,14 +258,44 @@ all$rounded <-round(all$distkm, digits=2)
 fill <- "snow3"
 colour <- "snow3"
 ggplot()+ 
-	geom_density(data=dist, aes(x=rounded), fill="snow3", position= "stack")+
-	geom_histogram(data=all, aes(x=rounded, fill=year, colour=year), alpha=.35, position="stack") + 
+	geom_density(data=dist, aes(x=distkm), fill="snow3")+
 			   theme_bw() + theme(panel.grid.major = element_blank(),
-			   panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text=element_text(family="Times New Roman", size= 20), 
-			   legend.position=c(.9, .85), legend.title=element_blank())+
-			   xlab("Distance (km)") + ylab("Frequency") + guides(fill=FALSE)+ scale_fill_brewer(palette="Set1")+
-		   	geom_vline(data=means, aes(xintercept=distkm, colour=year),
+			   panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text=element_text(family="Helvetica", size= 20), 
+			   legend.position=c(.9, .8), legend.title=element_blank())+ theme(legend.key = element_blank())+
+			   theme(panel.border = element_blank(),
+			   axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
+			   axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black")) +
+			   xlab("Distance (km)") + ylab("Probability Density") + guides(fill=FALSE)+ scale_fill_brewer(palette="Set1")+
+			   scale_x_continuous(limits = c(0,28), expand = c(0, 0)) +
+		   	geom_vline(data=allmean2, aes(xintercept=distkm, colour=year),
 		              linetype="dashed", size=1.5) + scale_colour_brewer(palette="Set1")
+#yearly dispersal, add to above plot if you want
+geom_histogram(data=all, aes(x=rounded, fill=year, colour=year), alpha=.35, position="stack") + 
+
+#plot mean dispersal density and annual dispersal mean lines
+ggplot()+ 
+	geom_density(data=all, aes(x=rounded), fill="snow3")+
+			   theme_bw() + theme(panel.grid.major = element_blank(),
+			   panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text=element_text(family="Helvetica", size= 20), 
+			   legend.position=c(.9, .8), legend.title=element_blank())+ theme(legend.key = element_blank())+
+			   theme(panel.border = element_blank(),
+			   axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
+			   axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black")) +
+			   xlab("Distance (km)") + ylab("Probability Density") + guides(fill=FALSE)+ scale_fill_brewer(palette="Set1")+
+			   scale_x_continuous(limits = c(0,28), expand = c(0, 0)) +
+		   	geom_vline(data=allmean2, aes(xintercept=distkm, colour=year),
+		              linetype="dashed", size=1.5) + scale_colour_brewer(palette="Set1")
+#plot annual density dispersal
+ggplot()+ 
+	geom_density(data=fifteen, aes(x=distkm), fill="red")+
+			   theme_bw() + theme(panel.grid.major = element_blank(),
+			   panel.grid.minor = element_blank(), axis.line = element_line(colour = "black"), text=element_text(family="Helvetica", size= 20), 
+			   legend.position=c(.9, .8), legend.title=element_blank())+ theme(legend.key = element_blank())+
+			   theme(panel.border = element_blank(),
+			   axis.line.x = element_line(size = 0.5, linetype = "solid", colour = "black"),
+			   axis.line.y = element_line(size = 0.5, linetype = "solid", colour = "black")) +
+			   xlab("Distance (km)") + ylab("Probability Density") + guides(fill=FALSE)+ scale_fill_brewer(palette="Set1")+
+			   scale_x_continuous(limits = c(0,28), expand = c(0, 0)) 
 
 #above plot is good
 #this plot below sucks
